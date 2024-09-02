d6a978210417864601b7d7d9a38f4a48cd448a8b
torrin1 <torrin@worx4you.com>
2024-09-02 14:04:17 -0400

    Simplifying some things

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import asyncio
 import websockets
 
@@ -9,31 +9,11 @@ async def connection(websocket, path):
         await websocket.send(response_message)
         print(f"Sent message: {response_message}")
 
-async def connect_to_remote_server(uri):
-    retry_backoff = 1  # Initial backoff time in seconds
-
-    while True:
-        try:
-            async with websockets.connect(uri) as websocket:
-                print(f"Connected to remote server at {uri}")
-                while True:
-                    await websocket.send("Test message from client")
-                    response = await websocket.recv()
-                    print(f"Client received: {response}")
-                    await asyncio.sleep(5)  # Sending message every 5 seconds
-        except ConnectionRefusedError:
-            print(f"Connection to {uri} failed, retrying in {retry_backoff} seconds...")
-            await asyncio.sleep(retry_backoff)
-            retry_backoff = min(retry_backoff * 2, 60)  # Exponential backoff, max 60 seconds
-
-async def server(port, remote_port):
+async def server(port):
     server_instance = await websockets.serve(connection, 'localhost', port)
     print(f"Server running on ws://localhost:{port}")
 
-    uri = f"ws://localhost:{remote_port}"
-    client_coroutine = connect_to_remote_server(uri)
-
-    await asyncio.gather(server_instance.wait_closed(), client_coroutine)
+    await asyncio.gather(server_instance.wait_closed())
 
     try:
         await server_instance.wait_closed()
@@ -45,13 +25,9 @@ async def server(port, remote_port):
         print("Shutdown complete.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <port> <remote_port>")
-        sys.exit(1)
+    port = os.getenv("port", 5000)
 
-    port = int(sys.argv[1])
-    remote_port = int(sys.argv[2])
     try:
-        asyncio.run(server(port, remote_port))
+        asyncio.run(server(port))
     except KeyboardInterrupt:
         print("WebSocket server interrupted and will be shut down.")
